c9197a6aeb977a15cc3948fecc8dc1d84ef948e0
Gorky Palacios Mutis <gorky.palacios@paymentevolution.com>
2024-12-26 19:32:02 -0500

    MiniCore Front

--- src/app/services/gastosService.js
+++ src/app/services/gastosService.js
@@ -1,6 +1,6 @@
 export const obtenerGastos = async (fechaInicio, fechaFin) => {
   const response = await fetch(
-    `http://localhost:5000/api/gastos?fechaInicio=${fechaInicio}&fechaFin=${fechaFin}`
+    `https://minicore-backend.onrender.com/api/gastos?fechaInicio=${fechaInicio}&fechaFin=${fechaFin}`
   );
   if (!response.ok) {
     throw new Error("Error al obtener los datos.");
